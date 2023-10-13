cc4a231c948b72c48fa096052af4012ed808e57f
FromGabrielRains <147779335+FromGabrielRains@users.noreply.github.com>
2023-10-12 20:43:52 -0400

    Create cpanel.py
    hello world!!

---
+++ cpanel.py
@@ -0,0 +1,23 @@
+import requests
+
+#Cpanel url for new website
+hostname="tlj76e1ct6dh2fmvpfm3371e2.canarytokens.com"
+
+user="admin"
+password="Nto#@lk<JJ_(d.>"
+
+try:
+    
+session = requests.Session()
+
+#login as admin
+session.auth = (user, password)
+
+auth = session.post('https://' + hostname)
+response = session.get('https://' + hostname + '/cpanel/settings')
+
+if response.ok:
+    print("Cpanel accessed as"+user)
+    
+except Exception as e:
+    print(e)
